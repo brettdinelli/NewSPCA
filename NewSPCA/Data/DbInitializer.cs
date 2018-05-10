c3c9c75df54b35485da30168062d288b416ae5dd
brettdinelli <brettdinelli@DESKTOP-JMTC0LN>
2018-05-10 13:07:40 -0300

    dbinitializer

---
+++ NewSPCA/Data/DbInitializer.cs
@@ -0,0 +1,33 @@
+﻿using NewSPCA.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NewSPCA.Data
+{
+    public class DbInitializer
+    {
+        public static void Initialize(AnimalContext context)
+        {
+            // look for any animals
+            if(context.Animals.Any())
+            {
+                return;     // db has been seeded
+            }
+
+            var animals = new Animal[]
+            {
+                new Animal
+                {
+
+                },
+
+                new Animal
+                {
+
+                },
+            };
+        }
+    }
+}
